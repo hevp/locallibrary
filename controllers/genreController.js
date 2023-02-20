5b616abd7e140f652baaac77e0fe5c4b82810eef
Hans van Piggelen <hevp101@outlook.com>
2023-02-20 14:05:52 +0100

    Implement genre update

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -7,7 +7,7 @@ const { body, validationResult } = require("express-validator");
 
 // Display list of all Genre.
 exports.genre_list = (req, res) => {
-    Genre.find()
+  Genre.find()
     .sort([["name", "ascending"]])
     .exec(function (err, list_genres) {
       if (err) {
@@ -23,35 +23,35 @@ exports.genre_list = (req, res) => {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = (req, res, next) => {
-    async.parallel(
-      {
-        genre(callback) {
-          Genre.findById(req.params.id).exec(callback);
-        },
-
-        genre_books(callback) {
-          Book.find({ genre: req.params.id }).exec(callback);
-        },
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
       },
-      (err, results) => {
-        if (err) {
-          return next(err);
-        }
-        if (results.genre == null) {
-          // No results.
-          const err = new Error("Genre not found");
-          err.status = 404;
-          return next(err);
-        }
-        // Successful, so render
-        res.render("genre_detail", {
-          title: "Genre Detail",
-          genre: results.genre,
-          genre_books: results.genre_books,
-        });
+
+      genre_books(callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
       }
-    );
-  };
+      if (results.genre == null) {
+        // No results.
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render("genre_detail", {
+        title: "Genre Detail",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
+};
 
 // Display Genre create form on GET.
 exports.genre_create_get = (req, res) => {
@@ -170,11 +170,64 @@ exports.genre_delete_post = (req, res) => {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+exports.genre_update_get = (req, res, next) => {
+  Genre.findById(req.params.id)
+    .exec((err, genre) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("genre_form", {
+        title: "Update genre",
+        genre
+      });
+    }
+  );
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-};
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ name: req.body.name }).exec((err, found_genre) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_genre) {
+          // Genre exists, redirect to its detail page.
+          res.redirect(found_genre.url);
+        } else {
+          Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+            if (err) {
+              return next(err);
+            }
+            // Genre saved. Redirect to genre detail page.
+            res.redirect(thegenre.url);
+          });
+        }
+      });
+    }
+  },
+];
